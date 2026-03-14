c17990ef0c6aa4c0bfd4613dffa4abc4e5869bcb
Marzena Pugo <94927552+MazPugo@users.noreply.github.com>
2026-03-14 21:40:59 +0000

    Delete projects directory

--- projects/cloud-support-engineer-projects/project-3/scripts/before_install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Ensure nginx is installed and deployment directory exists
-if [ ! -d /usr/share/nginx/html ]; then
-  mkdir -p /usr/share/nginx/html
-fi
-rm -rf /usr/share/nginx/html/*
